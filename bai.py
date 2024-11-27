3d09ec9034ad6df53b30b4d26c92694803173b20
bun <vlongbf@gmail.com>
2024-11-27 17:26:59 +0900

    課題１の提出

---
+++ bai.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+while True:
+    suuji = input("数字を入力してください: ")
+    
+    if suuji.isdigit():
+        kazu = int(suuji)
+        kekka = kazu * 2
+        print(suuji,"を二倍にすると",kekka,"になります。")
+        break
+    else:
+        print("それは数じゃねぇ")
+
