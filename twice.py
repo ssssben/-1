fa72e63fec4c095cf52a946bb55b44148ddb3489
bun <vlongbf@gmail.com>
2024-11-29 18:42:47 +0900

    非対話に変更

---
+++ twice.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# SPDX-FileCopyrightText: 2024 Ben
+# SPDX-License-Identifier: BSD-3-Clause
+
+import sys
+
+for line in sys.stdin:
+    suuji = line.strip()
+
+    if suuji.isdigit():
+        kazu = int(suuji)
+        kekka = kazu * 2
+        print(kekka)
+    else:
+        print("正確な数値ではありません。")
+
