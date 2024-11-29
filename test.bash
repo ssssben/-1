71da33902eb37f05d6f43ef4d3c5e705fb59710e
bun <vlongbf@gmail.com>
2024-11-29 19:33:35 +0900

    テスト内容を増やした

--- test.bash
+++ test.bash
@@ -9,7 +9,10 @@ set -x  # 詳細なコマンド表示
 script="twice.py"
 
 # 正常系テスト
-echo "5" | python3 "$script" | grep "5 を二倍にすると 10 になります。"
+echo "5" | python3 "$script" | grep "10"
+echo "0" | python3 "$script" | grep "0"
+echo "-3" | python3 "$script" | grep "-6"
+echo "100" | python3 "$script" | grep "200"
 
 # 異常系テスト
 echo "abc" | python3 "$script" | grep "正確な数値ではありません。"
